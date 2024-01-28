3d897125a28caed7d1010ba25ee23440abf9a100
ndonkoHenri <robotcoder4@protonmail.com>
2024-01-28 04:54:44 +0100

    created utils.py

---
+++ bot/utils.py
@@ -0,0 +1,32 @@
+import flet as ft
+
+
+class UserMessage(ft.Container):
+    def __init__(self, message):
+        super().__init__(
+            content=ft.Text(message),
+            bgcolor=ft.colors.BLUE,
+
+        )
+        self.message = message
+
+
+welcome_message = ft.Row(
+    alignment=ft.MainAxisAlignment.CENTER,
+    controls=[
+        ft.Container(
+            bgcolor=ft.colors.BLUE_400,
+            border_radius=10,
+            padding=15,
+            width=470,
+            height=110,
+            content=ft.Text(
+                "Hi, I am your personal assistant. :)\n"
+                "At the moment I can only search and play songs.\n"
+                "Want to try me? Use the Textfield below!",
+                size=18,
+                text_align=ft.TextAlign.CENTER
+            ),
+        )
+    ]
+)
